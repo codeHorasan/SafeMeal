51e65849a201d249b2e28629eebcb4afcc3d39dc
Uğur Horasan <ugur_horasan@outlook.com>
2020-11-28 17:27:34 +0100

    Add files via upload

---
+++ app/src/main/java/com/ugur/safemealdeneme/Classes/ProductBasket.java
@@ -0,0 +1,30 @@
+package com.ugur.safemealdeneme.Classes;
+
+public class ProductBasket {
+
+    private int productId, servings;
+
+    public ProductBasket(int productId, int servings) {
+        this.productId = productId;
+        this.servings = servings;
+    }
+
+    public ProductBasket(Object productId, Object servings) {
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+}
